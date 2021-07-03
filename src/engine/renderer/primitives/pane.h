c23caeb3223d7bd26fc42e2b134eaedaf05b5d58
EhwhoAmI <zyunlam@gmail.com>
2021-07-03 07:12:49 -0500

    First run at rendering to texture.

--- src/engine/renderer/primitives/pane.h
+++ src/engine/renderer/primitives/pane.h
@@ -8,5 +8,9 @@
 namespace conquerspace {
 namespace primitive {
 void MakeTexturedPaneMesh(conquerspace::engine::Renderable&);
+// Normal pane
+void MakeTexturedPaneMesh(conquerspace::engine::Mesh&);
+// In case you need a mirrored pane, like for framebuffers
+void MakeTexturedPaneMesh(conquerspace::engine::Mesh&, bool);
 }
 }
---
+++ src/engine/renderer/texturerenderer.cpp
@@ -0,0 +1,191 @@
+#include "texturerenderer.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <spdlog/spdlog.h>
+
+void conquerspace::engine::TextureRenderer::Draw() {
+    if (framebuffer == 0) {
+        glGenFramebuffers(1, &framebuffer);
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // make sure we clear the framebuffer's content
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    for (auto renderable : renderables) {
+        renderable->shaderProgram->UseProgram();
+        renderable->shaderProgram->setMat4("model", renderable->model);
+        renderable->shaderProgram->setMat4("view", view);
+        renderable->shaderProgram->setMat4("projection", projection);
+        int i = 0;
+        for (std::vector<conquerspace::asset::Texture*>::iterator it = renderable->textures.begin();
+                                                    it != renderable->textures.end(); ++it) {
+            glActiveTexture(GL_TEXTURE0 + i);
+            glBindTexture((*it)->texture_type, (*it)->id);
+            i++;
+        }
+
+        glBindVertexArray(renderable->mesh->VAO);
+        if (renderable->mesh->buffer_type == 1) {
+            glDrawElements(renderable->mesh->RenderType, renderable->mesh->indicies,
+                                                                        GL_UNSIGNED_INT, 0);
+        } else {
+            glDrawArrays(renderable->mesh->RenderType, 0, renderable->mesh->indicies);
+        }
+        glBindVertexArray(0);
+
+        // Reset active texture
+        glActiveTexture(GL_TEXTURE0);
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void conquerspace::engine::TextureRenderer::RenderBuffer() {
+    buffer_shader->UseProgram();
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, framebuffer);
+
+    glBindVertexArray(mesh_output->VAO);
+    if (mesh_output->buffer_type == 1) {
+        glDrawElements(mesh_output->RenderType, mesh_output->indicies, GL_UNSIGNED_INT, 0);
+    } else {
+        glDrawArrays(mesh_output->RenderType, 0, mesh_output->indicies);
+    }
+
+    glBindVertexArray(0);
+
+    // Reset active texture
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void conquerspace::engine::FramebufferRenderer::InitTexture() {
+    int width = 1280, height = 720;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a color attachment texture
+
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height); // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void conquerspace::engine::FramebufferRenderer::Clear() {
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void conquerspace::engine::FramebufferRenderer::BeginDraw() {
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+}
+
+void conquerspace::engine::FramebufferRenderer::EndDraw() {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void conquerspace::engine::FramebufferRenderer::RenderBuffer() {
+    buffer_shader.UseProgram();
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+
+    glBindVertexArray(mesh_output.VAO);
+    if (mesh_output.buffer_type == 1) {
+        glDrawElements(mesh_output.RenderType, mesh_output.indicies, GL_UNSIGNED_INT, 0);
+    } else {
+        glDrawArrays(mesh_output.RenderType, 0, mesh_output.indicies);
+    }
+
+    glBindVertexArray(0);
+
+    // Reset active texture
+    glActiveTexture(GL_TEXTURE0);
+}
+
+
+void conquerspace::engine::AAFrameBufferRenderer::InitTexture() {
+    const int SCR_WIDTH = 1280, SCR_HEIGHT = 720;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a multisampled color attachment texture
+    glGenTextures(1, &textureColorBufferMultiSampled);
+    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled);
+    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, SCR_WIDTH, SCR_HEIGHT, GL_TRUE);
+    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled, 0);
+    // create a (also multisampled) renderbuffer object for depth and stencil attachments
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        spdlog::info("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // configure second post-processing framebuffer
+    glGenFramebuffers(1, &intermediateFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, intermediateFBO);
+    // create a color attachment texture
+    glGenTextures(1, &screenTexture);
+    glBindTexture(GL_TEXTURE_2D, screenTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, screenTexture, 0);	// we only need a color buffer
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        spdlog::info("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void conquerspace::engine::AAFrameBufferRenderer::Clear() {
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void conquerspace::engine::AAFrameBufferRenderer::BeginDraw() {
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+}
+
+void conquerspace::engine::AAFrameBufferRenderer::EndDraw() {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void conquerspace::engine::AAFrameBufferRenderer::RenderBuffer() {
+    BeginDraw();
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO);
+    glBlitFramebuffer(0, 0, 1280, 720, 0, 0, 1280, 720, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+    EndDraw();
+    buffer_shader.UseProgram();
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, screenTexture);
+
+    glBindVertexArray(mesh_output.VAO);
+    if (mesh_output.buffer_type == 1) {
+        glDrawElements(mesh_output.RenderType, mesh_output.indicies, GL_UNSIGNED_INT, 0);
+    } else {
+        glDrawArrays(mesh_output.RenderType, 0, mesh_output.indicies);
+    }
+
+    glBindVertexArray(0);
+
+    // Reset active texture
+    glActiveTexture(GL_TEXTURE0);
+}
