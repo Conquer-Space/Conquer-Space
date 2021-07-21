c436c311cf8b200f650cc41fa5ee6613ceb0cc74
EhWhoAmI <zyunlam@gmail.com>
2021-07-21 07:19:45 -0500

    Make planet render on nvidia systems.

--- src/client/systems/sysstarsystemrenderer.cpp
+++ src/client/systems/sysstarsystemrenderer.cpp
@@ -474,14 +474,13 @@ conquerspace::client::systems::SysStarSystemRenderer::GeneratePlanetTexture(
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, image.GetWidth(), image.GetHeight(), 0, GL_RGBA,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.GetWidth(), image.GetHeight(), 0, GL_RGBA,
                           GL_UNSIGNED_INT_8_8_8_8, image.GetConstSlabPtr());
     glGenerateMipmap(GL_TEXTURE_2D);
-
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     return texture;
 }
 
