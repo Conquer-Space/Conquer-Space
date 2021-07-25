8a6c9101e7b42316f70c6b56264b41f3412985fb
EhWhoAmI <zyunlam@gmail.com>
2021-07-24 21:21:43 -0500

    asdf

--- modules/FindGLFW3.cmake
+++ modules/FindGLFW3.cmake
@@ -13,7 +13,7 @@ IF(WIN32)
 		DOC "The directory where GLFW/glfw3.h resides")
 
     FIND_LIBRARY( GLFW3_LIBRARY
-        NAMES glfw3 GLFW glfw
+        NAMES glfw3 GLFW glfw glfw3dll
         PATHS
         $ENV{PROGRAMFILES}/GLFW/lib
 		${GLFW_ROOT_DIR}/lib-vc2019
