555b5b2969efbe0d0f842efac8fbe9a144d3c9c2
EhWhoAmI <zyunlam@gmail.com>
2021-07-26 20:15:11 -0500

    Add license.

--- src/client/clientoptions.cpp
+++ src/client/clientoptions.cpp
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "engine/clientoptions.h"
+#include "client/clientoptions.h"
 
 void conquerspace::client::ClientOptions::LoadOptions(std::istream& inputstream) {
     Hjson::DecoderOptions decOpt;
