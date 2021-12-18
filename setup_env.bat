756b3ec3aaed255e4db9ca7927271ea16983cba3
EhWhoAmI <zyunlam@gmail.com>
2021-12-19 06:43:21 +0800

    Remove some outputs

--- setup_env.bat
+++ setup_env.bat
@@ -3,10 +3,8 @@ REM This file gets vcpkg, setups the cmake build system after that
 SET has_vcpkg=NOT exist vcpkg/
 if %has_vcpkg% (
     git clone https://github.com/Microsoft/vcpkg.git && call .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
-    echo %ERRORLEVEL%
 )
 
-echo Creating build files
 REM Build in static mode because it's cooler, also because google test doesn't support it
 REM and we'd like to build the tests
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
