560d4af53510ceb416a46ed2d011149d1ddf8ccf
EhWhoAmI <zyunlam@gmail.com>
2021-12-19 08:02:34 +0800

    Basic Auction system (#89)
    Auctions just determine the cheapest good and sell them, so far

---
+++ src/common/systems/economy/auctionhandler.h
@@ -0,0 +1,51 @@
+/* Conquer Space
+* Copyright (C) 2021 Conquer Space
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#include <entt/entt.hpp>
+
+#include "common/components/auction.h"
+
+namespace cqsp {
+namespace common {
+namespace systems {
+/// <summary>
+/// Buys a good from the market
+/// </summary>
+/// <param name="auction_house">Auction house to buy from</param>
+/// <param name="good">Good to buy</param>
+/// <param name="price">Price</param>
+/// <param name="quantity">Quantity</param>
+/// <returns>True if the order is fufilled immediately, false if a buy order is
+/// placed.</returns>
+bool BuyGood(components::AuctionHouse& auction_house, entt::entity good,
+             entt::entity agent, double price, double quantity);
+
+/// <summary>
+/// Sells a good to the market
+/// </summary>
+/// <param name="auction_house">Auction house to buy from</param>
+/// <param name="good">Good to buy</param>
+/// <param name="price">Price</param>
+/// <param name="quantity">Quantity</param>
+/// <returns>True if the order is fufilled immediately, false if a sell order is
+/// placed.</returns>
+bool SellGood(components::AuctionHouse& auction_house, entt::entity good,
+              entt::entity agent, double price, double quantity);
+}  // namespace systems
+}  // namespace common
+}  // namespace cqsp
