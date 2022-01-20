2512b40f70dd1db4648ee9f39cba19097135d368
EhWhoAmI <zyunlam@gmail.com>
2022-01-20 08:24:44 +0800

    Fix stb_vorbis giving warnings.

---
+++ lib/include/stb_vorbis.h
@@ -0,0 +1,28 @@
+/*
+* Copyright (C) 2021 Conquer Space
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#define STB_VORBIS_HEADER_ONLY
+#include "stb_vorbis.c"
+
+typedef unsigned char  uint8;
+typedef   signed char   int8;
+typedef unsigned short uint16;
+typedef   signed short  int16;
+typedef unsigned int   uint32;
+typedef   signed int    int32;
+
