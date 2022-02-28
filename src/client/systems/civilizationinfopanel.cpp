bcda954fcb77234e1552eb7dda7f45c2c74c8633
EhWhoAmI <zyunlam@gmail.com>
2022-02-28 19:31:38 +0800

    Basic civilization viewer

---
+++ src/client/systems/civilizationinfopanel.cpp
@@ -0,0 +1,62 @@
+/* Conquer Space
+ * Copyright (C) 2021 Conquer Space
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "client/systems/civilizationinfopanel.h"
+
+#include "common/components/player.h"
+#include "common/components/organizations.h"
+#include "common/components/economy.h"
+#include "common/util/utilnumberdisplay.h"
+
+void cqsp::client::systems::CivilizationInfoPanel::Init() {}
+
+void cqsp::client::systems::CivilizationInfoPanel::DoUI(int delta_time) {
+    // Get the ui
+    ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+    if (to_display) {
+        ImGui::SetNextWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x * 0.2,
+            ImGui::GetIO().DisplaySize.y * 0.4));
+    } else {
+        ImGui::SetNextWindowSize(ImVec2(-1, -1));
+    }
+    ImGui::Begin("Civilization Window");
+    if (ImGui::Button("Show/Hide Civilization")) {
+        to_display = !to_display;
+    }
+    if (to_display) {
+        CivInfoPanel();
+    }
+    ImGui::End();
+}
+
+void cqsp::client::systems::CivilizationInfoPanel::DoUpdate(int delta_time) {}
+
+void cqsp::client::systems::CivilizationInfoPanel::CivInfoPanel() {
+    ImGui::Text("Information");
+    // Get player
+    entt::entity player = GetUniverse()
+                              .view<common::components::Civilization,
+                                    common::components::Player>()
+                              .front();
+    ImGui::TextFmt("{}", player);
+    if (player == entt::null) {
+        return;
+    }
+    if (GetUniverse().any_of<common::components::Wallet>(player)) {
+        auto& wallet = GetUniverse().get<common::components::Wallet>(player);
+        ImGui::TextFmt("Reserves: {}", util::LongToHumanString(wallet.GetBalance()));
+    }
+}
