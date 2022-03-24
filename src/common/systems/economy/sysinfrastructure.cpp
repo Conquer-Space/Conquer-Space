656bdc7c2195d1bcc12730e89c64443cff59af4e
EhWhoAmI <zyunlam@gmail.com>
2022-03-24 21:00:54 +0800

    Rename filenames for a clearer specification for what each file does.

--- src/common/systems/economy/sysinfrastructure.cpp
+++ src/common/systems/economy/sysinfrastructure.cpp
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "common/systems/sysinfrastructure.h"
+#include "common/systems/economy/sysinfrastructure.h"
 
 #include "common/components/area.h"
 #include "common/components/infrastructure.h"
--- src/common/systems/economy/syspopulation.cpp
+++ src/common/systems/economy/syspopulation.cpp
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "common/systems/syspopulation.h"
+#include "common/systems/economy/syspopulation.h"
 
 #include <spdlog/spdlog.h>
 
