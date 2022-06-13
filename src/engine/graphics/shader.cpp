638b5caec5ccf64c23799f92f330dfb9e1a8331e
EhWhoAmI <zyunlam@gmail.com>
2022-06-14 07:51:14 +0800

    Add some diagnostics to shader program initialization. They are disabled by default.
    Fix #163 by reading uniform indices instead of uniform location.
    
    Refactor a a shaderprogram or two.

--- src/engine/graphics/shader.cpp
+++ src/engine/graphics/shader.cpp
@@ -18,7 +18,9 @@
 
 #include <glad/glad.h>
 
+#include <spdlog/spdlog.h>
 #include <vector>
+#include <map>
 
 unsigned int cqsp::asset::LoadShaderData(const std::string& code, int type) {
     unsigned int shader;
@@ -265,19 +267,180 @@ cqsp::asset::Shader::~Shader() {
         glDeleteShader(id);
     }
 }
-
-GLenum GetUniformType(int program, const char* name) {
-    int location = glGetUniformLocation(program, name);
+namespace {
+GLenum GetUniformType(GLuint program, const char* name) {
+    GLuint in[1];
+    glGetUniformIndices(program, 1, &name, in);
+    if (in == nullptr) {
+        return GL_INVALID_ENUM;
+    }
+    GLint location = *in;
+    SPDLOG_TRACE("Program {}: {} is {}", program, name, location);
     if (location != -1) {
         GLint size;
         GLenum type;
         GLsizei length;
-        glGetActiveUniform(program, location, 0, &length, &size, &type, NULL);
+        GLchar name[256];
+        glGetActiveUniform(program, *in, 255, &length, &size, &type, name);
+        SPDLOG_TRACE("{} {}", location, name);
         return type;
     } else {
         return GL_INVALID_ENUM;
     }
 }
+
+#if 0
+// We may need this in the future if we have to debug uniforms
+std::map<GLenum, std::string> type_set{
+    {GL_INVALID_ENUM, "invalid"},
+    {GL_FLOAT, "float"},
+    {GL_FLOAT_VEC2, "vec2"},
+    {GL_FLOAT_VEC3, "vec3"},
+    {GL_FLOAT_VEC4, "vec4"},
+    {GL_DOUBLE, "double"},
+    {GL_DOUBLE_VEC2, "dvec2"},
+    {GL_DOUBLE_VEC3, "dvec3"},
+    {GL_DOUBLE_VEC4, "dvec4"},
+    {GL_INT, "int"},
+    {GL_INT_VEC2, "ivec2"},
+    {GL_INT_VEC3, "ivec3"},
+    {GL_INT_VEC4, "ivec4"},
+    {GL_UNSIGNED_INT, "unsigned int"},
+    {GL_UNSIGNED_INT_VEC2, "uvec2"},
+    {GL_UNSIGNED_INT_VEC3, "uvec3"},
+    {GL_UNSIGNED_INT_VEC4, "uvec4"},
+    {GL_BOOL, "bool"},
+    {GL_BOOL_VEC2, "bvec2"},
+    {GL_BOOL_VEC3, "bvec3"},
+    {GL_BOOL_VEC4, "bvec4"},
+    {GL_FLOAT_MAT2, "mat2"},
+    {GL_FLOAT_MAT3, "mat3"},
+    {GL_FLOAT_MAT4, "mat4"},
+    {GL_FLOAT_MAT2x3, "mat2x3"},
+    {GL_FLOAT_MAT2x4, "mat2x4"},
+    {GL_FLOAT_MAT3x2, "mat3x2"},
+    {GL_FLOAT_MAT3x4, "mat3x4"},
+    {GL_FLOAT_MAT4x2, "mat4x2"},
+    {GL_FLOAT_MAT4x3, "mat4x3"},
+    {GL_DOUBLE_MAT2, "dmat2"},
+    {GL_DOUBLE_MAT3, "dmat3"},
+    {GL_DOUBLE_MAT4, "dmat4"},
+    {GL_DOUBLE_MAT2x3, "dmat2x3"},
+    {GL_DOUBLE_MAT2x4, "dmat2x4"},
+    {GL_DOUBLE_MAT3x2, "dmat3x2"},
+    {GL_DOUBLE_MAT3x4, "dmat3x4"},
+    {GL_DOUBLE_MAT4x2, "dmat4x2"},
+    {GL_DOUBLE_MAT4x3, "dmat4x3"},
+    {GL_SAMPLER_1D, "sampler1D"},
+    {GL_SAMPLER_2D, "sampler2D"},
+    {GL_SAMPLER_3D, "sampler3D"},
+    {GL_SAMPLER_CUBE, "samplerCube"},
+    {GL_SAMPLER_1D_SHADOW, "sampler1DShadow"},
+    {GL_SAMPLER_2D_SHADOW, "sampler2DShadow"},
+    {GL_SAMPLER_1D_ARRAY, "sampler1DArray"},
+    {GL_SAMPLER_2D_ARRAY, "sampler2DArray"},
+    {GL_SAMPLER_1D_ARRAY_SHADOW, "sampler1DArrayShadow"},
+    {GL_SAMPLER_2D_ARRAY_SHADOW, "sampler2DArrayShadow"},
+    {GL_SAMPLER_2D_MULTISAMPLE, "sampler2DMS"},
+    {GL_SAMPLER_2D_MULTISAMPLE_ARRAY, "sampler2DMSArray"},
+    {GL_SAMPLER_CUBE_SHADOW, "samplerCubeShadow"},
+    {GL_SAMPLER_BUFFER, "samplerBuffer"},
+    {GL_SAMPLER_2D_RECT, "sampler2DRect"},
+    {GL_SAMPLER_2D_RECT_SHADOW, "sampler2DRectShadow"},
+    {GL_INT_SAMPLER_1D, "isampler1D"},
+    {GL_INT_SAMPLER_2D, "isampler2D"},
+    {GL_INT_SAMPLER_3D, "isampler3D"},
+    {GL_INT_SAMPLER_CUBE, "isamplerCube"},
+    {GL_INT_SAMPLER_1D_ARRAY, "isampler1DArray"},
+    {GL_INT_SAMPLER_2D_ARRAY, "isampler2DArray"},
+    {GL_INT_SAMPLER_2D_MULTISAMPLE, "isampler2DMS"},
+    {GL_INT_SAMPLER_2D_MULTISAMPLE_ARRAY, "isampler2DMSArray"},
+    {GL_INT_SAMPLER_BUFFER, "isamplerBuffer"},
+    {GL_INT_SAMPLER_2D_RECT, "isampler2DRect"},
+    {GL_UNSIGNED_INT_SAMPLER_1D, "usampler1D"},
+    {GL_UNSIGNED_INT_SAMPLER_2D, "usampler2D"},
+    {GL_UNSIGNED_INT_SAMPLER_3D, "usampler3D"},
+    {GL_UNSIGNED_INT_SAMPLER_CUBE, "usamplerCube"},
+    {GL_UNSIGNED_INT_SAMPLER_1D_ARRAY, "usampler2DArray"},
+    {GL_UNSIGNED_INT_SAMPLER_2D_ARRAY, "usampler2DArray"},
+    {GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE, "usampler2DMS"},
+    {GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY, "usampler2DMSArray"},
+    {GL_UNSIGNED_INT_SAMPLER_BUFFER, "usamplerBuffer"},
+    {GL_UNSIGNED_INT_SAMPLER_2D_RECT, "usampler2DRect"},
+    {GL_IMAGE_1D, "image1D"},
+    {GL_IMAGE_2D, "image2D"},
+    {GL_IMAGE_3D, "image3D"},
+    {GL_IMAGE_2D_RECT, "image2DRect"},
+    {GL_IMAGE_CUBE, "imageCube"},
+    {GL_IMAGE_BUFFER, "imageBuffer"},
+    {GL_IMAGE_1D_ARRAY, "image1DArray"},
+    {GL_IMAGE_2D_ARRAY, "image2DArray"},
+    {GL_IMAGE_2D_MULTISAMPLE, "image2DMS"},
+    {GL_IMAGE_2D_MULTISAMPLE_ARRAY, "image2DMSArray"},
+    {GL_INT_IMAGE_1D, "iimage1D"},
+    {GL_INT_IMAGE_2D, "iimage2D"},
+    {GL_INT_IMAGE_3D, "iimage3D"},
+    {GL_INT_IMAGE_2D_RECT, "iimage2DRect"},
+    {GL_INT_IMAGE_CUBE, "iimageCube"},
+    {GL_INT_IMAGE_BUFFER, "iimageBuffer"},
+    {GL_INT_IMAGE_1D_ARRAY, "iimage1DArray"},
+    {GL_INT_IMAGE_2D_ARRAY, "iimage2DArray"},
+    {GL_INT_IMAGE_2D_MULTISAMPLE, "iimage2DMS"},
+    {GL_INT_IMAGE_2D_MULTISAMPLE_ARRAY, "iimage2DMSArray"},
+    {GL_UNSIGNED_INT_IMAGE_1D, "uimage1D"},
+    {GL_UNSIGNED_INT_IMAGE_2D, "uimage2D"},
+    {GL_UNSIGNED_INT_IMAGE_3D, "uimage3D"},
+    {GL_UNSIGNED_INT_IMAGE_2D_RECT, "uimage2DRect"},
+    {GL_UNSIGNED_INT_IMAGE_CUBE, "uimageCube"},
+    {GL_UNSIGNED_INT_IMAGE_BUFFER, "uimageBuffer"},
+    {GL_UNSIGNED_INT_IMAGE_1D_ARRAY, "uimage1DArray"},
+    {GL_UNSIGNED_INT_IMAGE_2D_ARRAY, "uimage2DArray"},
+    {GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE, "uimage2DMS"},
+    {GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE_ARRAY, "uimage2DMSArray"},
+    {GL_UNSIGNED_INT_ATOMIC_COUNTER, "atomic_uint"}};
+
+void eTB_GLSL__print_uniforms(GLuint program) {
+    GLint uniform_count;
+    glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &uniform_count);
+
+    GLchar name[256];
+
+    for (GLint i = 0; i < uniform_count; i++) {
+        memset(name, '\0', 256);
+        GLint size;
+        GLenum type;
+
+        glGetActiveUniform(program, i, 255, NULL, &size, &type, name);
+
+        GLint location = glGetUniformLocation(program, name);
+
+        auto& type_name = type_set[type];
+
+        if (size > 1) {
+            SPDLOG_INFO("Uniform {} (loc={}):\t{} {} <Size: {}>", i,
+                    location, type_name, name, size);
+        } else {
+            SPDLOG_INFO("Uniform {} (loc={}):\t{} {}", i, location, type_name,
+                   name);
+        }
+    }
+}
+
+#define debug_uniforms(program) eTB_GLSL__print_uniforms(program)
+#else
+#define debug_uniforms(program)(void) 0
+#endif
+std::map<Hjson::Type, std::string> hjson_type_set {
+    {Hjson::Type::Undefined, "Undefined"},
+    {Hjson::Type::Null, "Null"},
+    {Hjson::Type::Bool, "Bool"},
+    {Hjson::Type::Double, "Double"},
+    {Hjson::Type::Int64, "Int64"},
+    {Hjson::Type::String, "String"},
+    {Hjson::Type::Vector, "Vector"},
+    {Hjson::Type::Map, "Map"}
+};
+}  // namespace
 
 cqsp::asset::ShaderProgram_t cqsp::asset::ShaderDefinition::MakeShader() {
     cqsp::asset::Shader vert_shader(vert, cqsp::asset::ShaderType::VERT);
@@ -295,10 +458,10 @@ cqsp::asset::ShaderProgram_t cqsp::asset::ShaderDefinition::MakeShader() {
     }
     // Initial values
     shader->UseProgram();
-    // FIXME(EhWhoAmI): Error handling and verification if the variablesis greatly needed
-    // are the right type is greatly needed.
+    debug_uniforms(shader->program);
+
     for (auto value : uniforms) {
-        int type = GetUniformType(shader->program, value.first.c_str());
+        GLenum type = GetUniformType(shader->program, value.first.c_str());
         switch (type) {
             case GL_SAMPLER_1D:
             case GL_SAMPLER_2D:
@@ -306,30 +469,68 @@ cqsp::asset::ShaderProgram_t cqsp::asset::ShaderDefinition::MakeShader() {
             case GL_SAMPLER_CUBE:
             case GL_SAMPLER_1D_SHADOW:
             case GL_SAMPLER_2D_SHADOW:
+            case GL_INT:
+                // Type has to be int
+                if (value.second.type() != Hjson::Type::Int64) {
+                    SPDLOG_WARN("Uniform {} is not type int, it is {}",
+                                value.first,
+                                hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (int)value.second.to_int64());
                 break;
             case GL_FLOAT_VEC2:
+                if (value.second.type() != Hjson::Type::Vector &&
+                    value.second.size() != 2) {
+                    SPDLOG_WARN("Uniform {} is not type vec2, it is {}",
+                                value.first,
+                                hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (float)value.second[0].to_double(),
                             (float)value.second[1].to_double());
                 break;
             case GL_FLOAT_VEC3:
+                if (value.second.type() != Hjson::Type::Vector &&
+                    value.second.size() != 3) {
+                    SPDLOG_WARN("Uniform {} is not type vec3, it is {}",
+                                value.first,
+                                hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (float)value.second[0].to_double(),
                             (float)value.second[1].to_double(),
                             (float)value.second[2].to_double());
                 break;
             case GL_FLOAT_VEC4:
+                if (value.second.type() != Hjson::Type::Vector &&
+                    value.second.size() != 4) {
+                    SPDLOG_WARN("Uniform {} is not type vec4, it is {}",
+                                value.first,
+                                hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (float)value.second[0].to_double(),
                             (float)value.second[1].to_double(),
                             (float)value.second[2].to_double(),
                             (float)value.second[3].to_double());
                 break;
             case GL_FLOAT:
+                if (!(value.second.type() == Hjson::Type::Double ||
+                    value.second.type() == Hjson::Type::Int64)) {
+                    SPDLOG_WARN("Uniform {} is not type float, it is {}",
+                        value.first, hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (float)value.second.to_double());
                 break;
-            case GL_INT:
-                shader->Set(value.first, (int)value.second.to_int64());
-                break;
             case GL_BOOL:
+                if (value.second.type() != Hjson::Type::Bool) {
+                    SPDLOG_WARN("Uniform {} is not type bool, it is {}",
+                                value.first,
+                                hjson_type_set[value.second.type()]);
+                    break;
+                }
                 shader->Set(value.first, (bool)value.second);
                 break;
             // We don't support matrices
