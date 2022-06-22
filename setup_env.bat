f9ebd9dfb1e72bc42a9924c0fbbb38a686e84dc5
EhWhoAmI <zyunlam@gmail.com>
2022-06-22 08:08:31 +0800

    Change things

--- setup_env.bat
+++ setup_env.bat
@@ -2,6 +2,7 @@
 REM This file gets vcpkg, setups the cmake build system after that
 SET has_vcpkg=NOT exist vcpkg/vcpkg.exe
 if %has_vcpkg% (
+    git submodule update vcpkg
     call .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
 )
 REM Build in static mode because it's cooler, also because google test doesn't support it
--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+git submodule update vcpkg
 ./vcpkg/bootstrap-vcpkg.sh
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
