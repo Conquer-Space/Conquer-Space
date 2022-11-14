ece6a80921ba7072bc848edffff336779da7a112
EhWhoAmI <zyunlam@gmail.com>
2022-11-14 08:19:31 +0800

    Add automatic clang formatting for files

--- tools/clang_format.py
+++ tools/clang_format.py
@@ -6,19 +6,25 @@ import subprocess
 dirs = ['src', 'test']
 extensions = ['h', 'cpp']
 
+def is_cpp_file(file):
+    for k in extensions:
+        if file.endswith('.' + k):
+            return True
+    return False
+
+def format_file(file):
+    subprocess.run(['clang-format', '-i', file])
+
+# Scans entire directory and runs clang format on it
 def scan(dir):
     for path, subdirs, files in os.walk(dir):
         for name in files:
             pt = str(os.path.join(path, name))
-            to_break = True
-            for k in extensions:
-                if pt.endswith('.' + k):
-                    to_break = False
-            if to_break:
+            if not is_cpp_file(pt):
                 continue
             # then run clang format
             print(pt)
-            subprocess.run(['clang-format', '-i', pt])
+            format_file(pt)
 
 
 if __name__ == "__main__":
