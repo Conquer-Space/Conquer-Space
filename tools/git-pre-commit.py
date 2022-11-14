3edadb971b6f5628a7c0adad47354870d6ca5bba
EhWhoAmI <zyunlam@gmail.com>
2022-11-14 08:43:40 +0800

    * Clang-format python file helper * Add cpplint.cfg * Edit clang-format * Add cpplint option

--- tools/git-pre-commit.py
+++ tools/git-pre-commit.py
@@ -10,9 +10,7 @@ for line in out.splitlines():
     if (line is None):
         continue
 
-    val = subprocess.run(['cpplint', '--quiet', '--linelength=120', '--recursive',
-    '--filter=-readability/casting,-runtime/references,-build/c++11,-readability/check,-whitespace/comments,-runtime/int',
-    '--verbose=0', line], stdout=subprocess.PIPE)
+    val = subprocess.run(['cpplint', '--quiet', '--recursive', line], stdout=subprocess.PIPE)
     if val.returncode != 0:
         error = True
 
