394e1071b332708d0a85850f97d9b360a2aec805
EhWhoAmI <zyunlam@gmail.com>
2022-11-14 08:19:31 +0800

    Add automatic clang formatting for files

--- tools/git-pre-commit.py
+++ tools/git-pre-commit.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import clang_format
 
 output = subprocess.run(['git', 'diff', '--cached', '--name-only', '--diff-filter=ACM'], stdout=subprocess.PIPE)
 out = output.stdout.decode('utf-8')
@@ -9,6 +10,13 @@ for line in out.splitlines():
     # Run cpplint for the files
     if (line is None):
         continue
+    if not clang_format.is_cpp_file(line):
+        continue
+    # Needs to be cpp file
+    # Clang format the files
+    clang_format.format_file(line)
+    # Add file
+    subprocess.run(['git', 'add', line])
 
     val = subprocess.run(['cpplint', '--quiet', '--recursive', line], stdout=subprocess.PIPE)
     if val.returncode != 0:
