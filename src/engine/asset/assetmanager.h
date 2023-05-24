c133bf0999c12d662eba8fa24add0a367e92fcbc
EhWhoAmI <zyunlam@gmail.com>
2023-05-24 16:43:16 +0800

    Update licenses on all files to match up the year

--- src/engine/asset/assetmanager.h
+++ src/engine/asset/assetmanager.h
@@ -1,19 +1,19 @@
 /* Conquer Space
-* Copyright (C) 2021 Conquer Space
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2021-2023 Conquer Space
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #pragma once
 
 #include <hjson.h>
@@ -63,8 +63,8 @@ class AssetPrototype {
 };
 
 /**
-* Holds a prototype in the queue to be loaded in the main thread
-*/
+ * Holds a prototype in the queue to be loaded in the main thread
+ */
 class QueueHolder {
  public:
     QueueHolder() { prototype = nullptr; }
@@ -74,8 +74,8 @@ class QueueHolder {
 };
 
 /**
-* Queue to hold the asset prototypes.
-*/
+ * Queue to hold the asset prototypes.
+ */
 template <typename T>
 class ThreadsafeQueue {
     std::queue<T> queue_;
