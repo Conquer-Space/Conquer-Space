97f011a9a1809da0000d8b96b8fdea942d65d959
EhWhoAmI <eh@ehwhoami.com>
2023-09-04 00:34:07 -0500

    Add clang tidy to hook for windows.
    Improve hook install in case we have any changes

--- tools/git-pre-commit.py
+++ tools/git-pre-commit.py
@@ -1,26 +1,66 @@
+# You can create the file `git-hook.cfg` in the root directory to activate clang tidy for the hook.
+# Just config it with:
+# ```
+# [CLANG_TIDY]
+# clang_tidy = yes
+# ```
+# The hook will automatically fix any issues that show up
 import subprocess
 import sys
 import clang_format
+import configparser
+import requests
+import os
 
 output = subprocess.run(['git', 'diff', '--cached', '--name-only', '--diff-filter=ACM'], stdout=subprocess.PIPE)
-out = output.stdout.decode('utf-8')
+git_diff_list = output.stdout.decode('utf-8')
+
+if len(git_diff_list) == 0:
+    exit(0)
 
 error = False
-for line in out.splitlines():
-    # Run cpplint for the files
-    if (line is None):
-        continue
-    if not clang_format.is_cpp_file(line):
-        continue
-    # Needs to be cpp file
+
+cpp_file_list = [s for s in git_diff_list.splitlines() if s is not None and clang_format.is_cpp_file(s)]
+for line in cpp_file_list:
     # Clang format the files
     clang_format.format_file(line)
-    # Add file
-    subprocess.run(['git', 'add', line])
 
+    # Run cpplint for the files
     val = subprocess.run(['cpplint', '--quiet', '--recursive', line], stdout=subprocess.PIPE)
     if val.returncode != 0:
         error = True
+
+
+cfg = configparser.ConfigParser()
+try:
+    cfg.read('git-hook.cfg')
+except:
+    pass
+
+RUN_CLANG_TIDY_URL='https://raw.githubusercontent.com/llvm/llvm-project/release/17.x/clang-tools-extra/clang-tidy/tool/run-clang-tidy.py'
+CLANG_TIDY_PATH='tools/run-clang-tidy.py'
+if 'CLANG_TIDY' in cfg and 'clang_tidy' in cfg['CLANG_TIDY']:
+    # We don't do linux for now
+    if cfg['CLANG_TIDY']['clang_tidy'] == 'yes' and sys.platform == 'win32':
+        print('Generating \'compile_commands.json\'')
+        subprocess.run('"tools/makefile_gen.bat"')
+        # run makefile
+        if not os.path.exists(CLANG_TIDY_PATH):
+            request = requests.get(RUN_CLANG_TIDY_URL)
+            with open(CLANG_TIDY_PATH, 'wb') as f:
+                f.write(request.content)
+        # Now run clang-tidy fix
+        print('Running clang tidy')
+        # Get the git diff files
+        
+        if len(cpp_file_list) > 0:
+            s = " ".join(cpp_file_list)
+            val = subprocess.run('python tools/run-clang-tidy.py -config-file=.clang-tidy -quiet -fix -p build-makefile ' + s)
+            if val.returncode != 0:
+                error = True
+
+for line in cpp_file_list:
+    subprocess.run(['git', 'add', line])
 
 if error:
     sys.exit(1)
