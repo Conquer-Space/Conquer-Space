cb62fcad52a5a84fdcc5d83f9a0dcc2b6a6cd92f
EhWhoAmI <eh@ehwhoami.com>
2023-09-04 00:34:07 -0500

    Add clang tidy to hook for windows.
    Improve hook install in case we have any changes

--- tools/install_hook.py
+++ tools/install_hook.py
@@ -6,14 +6,9 @@ if not os.path.exists(".git/hooks"):
     print("Not a git folder. Please run this from the top level directory of the project")
     exit()
 
-
 # Check if they installed anything that they need
 if shutil.which("cpplint") is None:
     print("Cpplint was not installed, run `pip install cpplint` for the hook to function correctly")
-    exit()
-
-if os.path.exists(".git/hooks/pre-commit"):
-    print("Hook already exists, our custom hook is presumed to be installed")
     exit()
 
 shutil.copy("tools/pre-commit", ".git/hooks/pre-commit")
