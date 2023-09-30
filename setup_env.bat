a53c77708795da24d9a792b02cd93c7628f84ab3
EhWhoAmI <eh@ehwhoami.com>
2023-09-29 22:51:33 -0500

    More fixes

--- setup_env.bat
+++ setup_env.bat
@@ -8,4 +8,4 @@ if %has_vcpkg% (
 )
 REM Build in static mode because it's cooler, also because google test doesn't support it
 REM and we'd like to build the tests
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static %*
+cmake -B build %*
