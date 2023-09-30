ab7ec164f8a2ae2ac3256894ae31ec00828ad21d
EhWhoAmI <eh@ehwhoami.com>
2023-09-29 23:24:57 -0500

    Some build improvements for easier compilation. (#233)

--- setup_env.bat
+++ setup_env.bat
@@ -2,12 +2,10 @@
 REM This file gets vcpkg, setups the cmake build system after that
 SET has_vcpkg=NOT exist vcpkg/vcpkg.exe
 git -C vcpkg fetch --unshallow
-git -C vcpkg describe --tags
-git -C vcpkg rev-parse --short HEAD
 
 if %has_vcpkg% (
     call .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
 )
 REM Build in static mode because it's cooler, also because google test doesn't support it
 REM and we'd like to build the tests
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static %*
+cmake -B build %*
--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,4 @@ git -C vcpkg describe --tags
 git -C vcpkg rev-parse --short HEAD
 
 ./vcpkg/bootstrap-vcpkg.sh
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -B build
