73654e7d430b346c3abe749fe10c1dba373d299c
EhWhoAmI <eh@ehwhoami.com>
2024-11-10 23:01:45 -0600

    Add work

---
+++ src/common/systems/loading/loadorbit.h
@@ -0,0 +1,27 @@
+/* Conquer Space
+ * Copyright (C) 2021-2023 Conquer Space
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <hjson.h>
+
+#include <optional>
+
+#include "common/components/orbit.h"
+
+namespace cqsp::common::systems::loading {
+std::optional<components::types::Orbit> LoadOrbit(const Hjson::Value& values);
+}  // namespace cqsp::common::systems::loading
