d0e772f7f26237deb355a1c112b105cabdd852c5
EhWhoAmI <eh@ehwhoami.com>
2024-11-19 14:44:26 -0600

    Add satellite parser

--- tools/download_satellites.py
+++ tools/download_satellites.py
@@ -2,15 +2,36 @@ import requests
 import hjson
 import parse_tle
 import sys
+import argparse
+
+tle_source = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
 
 if __name__ == "__main__":
-    tle_source = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
-    if len(sys.argv) > 1:
-        tle_source = sys.argv[1]
+    parser = argparse.ArgumentParser(prog="Satellite Parser")
+    parser.add_argument("-i", "--input", default=tle_source, help="Input text file url of the TLE input")
+    parser.add_argument("-o", "--output", default=None, help="Output source. If left empty will output to stdout")
+    args = parser.parse_args()
+
+    tle_source = args.input
+    print("Reading from", tle_source)
     # Get all space stations
     content = requests.get(tle_source, stream=True).content
     lines = content.decode().splitlines()
-    print(lines)
     satellites = [parse_tle.parse_tle(lines[k:k+3]) for k in range(0, len(lines), 3)]
-    print(hjson.dumps(satellites))
-
+    # Also add the center bodies for each satellite?
+    satellite_entities = []
+    for sat in satellites:
+        sat["reference"] = "earth"
+        name = sat["name"]
+        del sat["name"]
+        di = {
+            "name": name,
+            "orbit": sat,
+            "tags": []
+        }
+        satellite_entities.append(di)
+    if args.output is None:
+        print(hjson.dumps(satellite_entities))
+    else:
+        with open(args.output, "w") as f:
+            hjson.dump(satellite_entities, f)
