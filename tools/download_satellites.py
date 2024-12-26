206ff9988a3d059e97663c3001085483fee346d1
EhWhoAmI <eh@ehwhoami.com>
2024-12-26 14:09:29 +0800

    Cleanup

--- tools/download_satellites.py
+++ tools/download_satellites.py
@@ -11,12 +11,14 @@ tle_source = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=
 #       -o ../binaries/data/core/data/planet_data/earth/satellites/NAVSTAR.hjson
 # -i "https://celestrak.org/NORAD/elements/gp.php?GROUP=galileo&FORMAT=tle" -t "navigation galileo"
 #       -o ../binaries/data/core/data/planet_data/earth/satellites/galileo.hjson
-# 
+# -i "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle" -t "station"
+#       -o ../binaries/data/core/data/planet_data/earth/satellites/stations.hjson
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Satellite Parser")
     parser.add_argument("-i", "--input", default=tle_source, help="Input text file url of the TLE input")
     parser.add_argument("-o", "--output", default=None, help="Output source. If left empty will output to stdout")
     parser.add_argument("-t", "--tags", default=None, help="Tags to add to each satellite")
+    parser.add_argument("-m", "--model", default=None, help="Model to set each satellite")
     args = parser.parse_args()
 
     tle_source = args.input
@@ -49,6 +51,8 @@ if __name__ == "__main__":
             "orbit": orbit,
             "tags": tags
         }
+        if args.model:
+            di["model"] = args.model
         satellite_entities.append(di)
     if args.output is None:
         print(hjson.dumps(satellite_entities))
