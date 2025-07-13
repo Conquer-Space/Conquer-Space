cac2e52cc002b03b85f036c329f63f8733b9cf64
EhWhoAmI <eh@ehwhoami.com>
2025-07-12 23:41:15 -0700

    Fix bug with headless mode

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,8 @@
 
 int main(int argc, char* argv[]) {
     bool headless = false;
-    for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "--headless") != 0) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--headless") == 0) {
             // Load headless application
             headless = true;
         }
