ea4ade2b8a8104d59ec9bcdcb11344de7023d732
agm-114 <agm.114.email@gmail.com>
2025-08-23 11:15:33 +0530

    move fields and fix ref

--- src/common/systems/loading/fields.cpp
+++ src/common/systems/loading/fields.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "common/systems/science/fields.h"
+#include "common/systems/loading/fields.h"
 
 #include <spdlog/spdlog.h>
 
