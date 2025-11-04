9b102671d2e4cdb81bd1618eda6244ce4097ce8a
agm-114 <agm.114.email@gmail.com>
2025-11-04 23:25:58 +0530

    fix build error

--- src/common/actions/science/technologyactions.cpp
+++ src/common/actions/science/technologyactions.cpp
@@ -35,7 +35,7 @@ void ResearchTech(Node& civilization, Node& tech) {
     }
 }
 
-void ProcessAction(Node& civilization, std::string& action) {
+void ProcessAction(Node& civilization, const std::string& action) {
     // Process the tech
     // Split by the colon
     auto& tech_progress = civilization.get_or_emplace<components::science::TechnologicalProgress>();
