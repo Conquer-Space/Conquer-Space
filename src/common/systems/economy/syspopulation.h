740767b6b0c7c86bafc85564e3b3882e8d3fb46e
agm-114 <agm.114.email@gmail.com>
2025-11-04 11:01:38 +0530

    nodify syspopulation

--- src/common/systems/economy/syspopulation.h
+++ src/common/systems/economy/syspopulation.h
@@ -16,6 +16,7 @@
  */
 #pragma once
 
+#include "common/components/resource.h"
 #include "common/systems/isimulationsystem.h"
 
 namespace cqsp::common::systems {
@@ -31,5 +32,9 @@ class SysPopulationConsumption : public ISimulationSystem {
     explicit SysPopulationConsumption(Game& game) : ISimulationSystem(game) {}
     void DoSystem() override;
     int Interval() override { return components::StarDate::DAY; }
+
+ private:
+    void ProcessSettlement(Node settlement, components::ResourceConsumption& marginal_propensity_base,
+                           components::ResourceConsumption& autonomous_consumption_base, float savings);
 };
 }  // namespace cqsp::common::systems
