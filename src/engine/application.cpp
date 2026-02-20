6ab0eea2b22e942d6c2ae986307872c32d64a5e5
EhWhoAmI <eh@ehwhoami.com>
2026-02-19 19:54:12 -0600

    Fix change

--- src/engine/application.cpp
+++ src/engine/application.cpp
@@ -549,7 +549,7 @@ void Application::LogInfo() {
 #ifndef NDEBUG
     ENGINE_LOG_INFO("Conquer Space Debug {}", CQSP_VERSION_STRING);
 #else
-    ENGINE_LOG_INFO("Conquer Space {} {}", CQSP_VERSION_STRING);
+    ENGINE_LOG_INFO("Conquer Space {}", CQSP_VERSION_STRING);
 #endif
     ENGINE_LOG_INFO("Platform: {}", PLATFORM_NAME);
     ENGINE_LOG_INFO("Compiled {} {}", __DATE__, __TIME__);
